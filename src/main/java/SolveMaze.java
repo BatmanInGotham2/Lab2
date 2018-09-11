341ddf2cb23787bca70341dcc7ad903ed2fc0328
BatmanInGotham2 <gautam@ajjarapu.com>
2018-09-11 17:30:53 -0500

    Added Maze solving functionality.

--- src/main/java/SolveMaze.java
+++ src/main/java/SolveMaze.java
@@ -34,9 +34,21 @@ public class SolveMaze {
          * Feel free to adjust this number if you experiment with other mazes.
          */
         for (int step = 0; step < 1000; step++) {
-            // Implement your maze solving algorithm here
-        }
+            if (!maze.isFinished()) {
+                maze.turnRight();
+                if(maze.canMove())
+                {
+                    maze.move();
+                } else {
 
+                    while(!maze.canMove()) {
+                        maze.turnLeft();
+                    }
+                    maze.move();
+                }
+
+            }
+        }
         if (maze.isFinished()) {
             System.out.println("You solved the maze!");
         } else {
